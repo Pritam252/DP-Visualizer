74f8b48898c20dda6d6641c946f3a4f8cf4399c2
LordShangbin <89254359+LordShangbin@users.noreply.github.com>
2026-03-18 21:39:42 +0700

    update1

--- DP_2D_test.cpp
+++ DP_2D_test.cpp
@@ -1,35 +1,23 @@
 #include <bits/stdc++.h>
 #include "Visualizer.cpp"
-
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
 
 const int maxn = 1005, modnum = 1e9 + 7;
 int n, m;
-pii treasure;
 
 int main(){
     cin.tie(nullptr)->sync_with_stdio(0);
     cin >> n >> m;
     vector<vector<int>> dp(n+1, vector<int>(m+1));
-    init(dp, {1, dp.size()}, {1, dp[0].size()});
-    treasure = {3, 3};
+    init(dp, {1, n+1}, {1, m+1});
     dp[0][1] = 1;
     Describe("dp[0] = 1");
     Visualize(dp, {}, {});
-    for (int i = 1; i <= treasure.first; i++){
-        for (int j = 1; j <= treasure.second; j++){
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
             dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % modnum;
             Describe("dp[i]");
-            Visualize(dp, {}, {});
-        }
-    }
-    for (int i = treasure.first; i <= n; i++){
-        for (int j = treasure.second; j <= m; j++){
-            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % modnum;
-            Describe("dp[i]");
-            Visualize(dp, {}, {});
+            Visualize(dp, {{i - 1, j}, {i, j - 1}}, {{i, j}});
         }
     }
     cout << dp[n][m];
--- Visualizer.cpp
+++ Visualizer.cpp
@@ -11,10 +11,16 @@ private:
     Font Arial;
     std::vector<int> global_dp;
     std::string Description;
-    std::pair<int, int> printing_range[2]; //[0] = i, [1] = j
-    int range_length[2];
-    float box_size = 120, box_dist = 30;
-    float font_size = 40;
+    struct {
+        std::pair<int, int> i;
+        std::pair<int, int> j;
+    } printing_range; 
+    struct {
+        int i;
+        int j;
+    } range_length; 
+
+    float box_size, box_dist, font_size;
     
     void DrawArrow(Vector2 start, Vector2 end, Color color) {
         DrawLineEx(start, end, 7.0f, color);
@@ -35,13 +41,16 @@ public:
     void init(const std::vector<int>& dp, std::pair<int, int> range) {
         SetTraceLogLevel(LOG_ERROR);
         if (initialized) return;
-        printing_range[0] = range;
-        range_length[0] = range.second - range.first;
-        if (range_length[0] > 15 || range_length[0] <= 0 || dp.empty()) {
+        printing_range.i = range;
+        range_length.i = range.second - range.first;
+        if (range_length.i > 15 || range_length.i <= 0 || dp.empty()) {
             std::cerr << "DP size overflow or invalid, Cant Visualize\n";
             return;
         }
-        screen_width = 100 + (range_length[0] * (box_size + box_dist)); 
+        box_size = 120;
+        box_dist = 30;
+        font_size = 40;
+        screen_width = 100 + (range_length.i * (box_size + box_dist)); 
         screen_height = 500;
         InitWindow(screen_width, screen_height, "Single Dimension DP Visualizer");
         SetTargetFPS(60); 
@@ -51,44 +60,44 @@ public:
     void update(const std::vector<int>& dp, std::vector<int>& prev, std::vector<int>& cur) {
         if (!initialized) return;
         bool next_step = false;
-        auto [start, stop] = printing_range[0];
-        std::vector<float> pos_x(range_length[0]);
+        auto [start, stop] = printing_range.i;
+        std::vector<float> pos_x(range_length.i);
         float pos_y = 200.0f;
         while (!WindowShouldClose() && !next_step) {
             BeginDrawing();
             ClearBackground(RAYWHITE);
             //Calculate position
-            for (int i = 0; i < range_length[0]; i++) {
+            for (int i = 0; i < range_length.i; i++) {
                 pos_x[i] = 75.0f + ((box_size + box_dist) * i);
             }
             //Draw Blue Box (Normal)
-            for (int i = 0; i < range_length[0]; i++) {
+            for (int i = 0; i < range_length.i; i++) {
                 auto true_i = i + start;
                 Color color = { 170, 220, 255, 255 }; //Light Blue
                 DrawRectangleRounded({pos_x[i], pos_y, box_size, box_size}, 0.2, 6, color);
-                DrawRectangleRoundedLines({pos_x[i], pos_y, box_size, box_size}, 0.2, 10, PURPLE);
+                DrawRectangleRoundedLinesEx({pos_x[i], pos_y, box_size, box_size}, 0.2, 10, 3, DARKBLUE);
             }
             //Draw green box (current dp)
             for (auto true_i : cur){
                 int i = true_i - start;
-                if (i >= range_length[0] || i < 0) continue; //Out of bound
+                if (i >= range_length.i || i < 0) continue; //Out of bound
                 Color color = { 170, 255, 220, 255 }; //Light green
                 DrawRectangleRounded({pos_x[i], pos_y, box_size, box_size}, 0.2, 6, color);
             }
             //Draw purple box (previos dp)
             for (auto true_i : prev){
                 int i = true_i - start;
-                if (i >= range_length[0] || i < 0) continue; //Out of bound
+                if (i >= range_length.i || i < 0) continue; //Out of bound
                 Color color = { 207, 159, 255, 255 }; //Light Violet
                 DrawRectangleRounded({pos_x[i], pos_y, box_size, box_size}, 0.2, 6, color);
             }
             //Draw Text (dp value)
-            for (int i = 0; i < range_length[0]; i++) {
+            for (int i = 0; i < range_length.i; i++) {
                 int true_i = i + start;
                 std::string val = std::to_string(dp[true_i]);
-                int textWidth = MeasureText(val.c_str(), 50);
+                int textWidth = MeasureText(val.c_str(), font_size);
                 DrawTextEx(Arial, val.c_str(), {pos_x[i] + (box_size/2 - textWidth/2), pos_y + (box_size/3)}, font_size, 5, BLACK);
-                DrawTextEx(Arial, TextFormat("%d", true_i), {pos_x[i] + box_size/2, pos_y - 30}, 30, 3, DARKGRAY);
+                DrawTextEx(Arial, TextFormat("%d", true_i), {pos_x[i] + box_size/2 - 10, pos_y - 30}, 30, 3, DARKGRAY);
             }
             //Draw Description
             if (!Description.empty()) {
@@ -103,42 +112,79 @@ public:
     void init2D(const std::vector<std::vector<int>>& dp, std::pair<int, int> range_i, std::pair<int, int> range_j) {
         SetTraceLogLevel(LOG_ERROR);
         if (initialized) return;
-        printing_range[0] = range_i;
-        printing_range[1] = range_j;
-        range_length[0] = range_i.second - range_i.first;
-        range_length[1] = range_j.second - range_j.first;
-        if (dp.empty() || dp.at(0).empty() || range_length[0] > 15 || range_length[0] <= 0 || range_length[1] > 10 || range_length[1] <= 0) {
+        printing_range.i = range_i;
+        printing_range.j = range_j;
+        range_length.i = range_i.second - range_i.first;
+        range_length.j = range_j.second - range_j.first;
+        if (dp.empty() || dp.at(0).empty() || range_length.i > 15 || range_length.i <= 0 || range_length.j > 10 || range_length.j <= 0) {
             std::cerr << "DP size overflow or invalid, Cant Visualize\n";
             return;
         }
-        screen_width = 100 + (range_length[0] * (box_size + box_dist)); 
-        screen_height = 200 + (range_length[1] * (box_size + box_dist));
+        box_size = 100;
+        box_dist = 30;
+        font_size = 40;
+        screen_width = 100 + (range_length.j * (box_size + box_dist)); 
+        screen_height = 200 + (range_length.i * (box_size + box_dist));
         InitWindow(screen_width, screen_height, "Double Dimension DP Visualizer");
         SetTargetFPS(60); 
-        initialized = true;
         Arial = LoadFontEx("C:\\Windows\\Fonts\\arial.ttf", 64, NULL, 0);
+        initialized = true;
     }
     void update2D(const std::vector<std::vector<int>>& dp, std::vector<std::pair<int, int>>& prev, std::vector<std::pair<int, int>>& cur) {
         if (!initialized) return;
         bool next_step = false;
-        
-        auto [start_i, stop_i] = printing_range[0];
-        auto [start_j, stop_j] = printing_range[1];
+        auto [start_i, stop_i] = printing_range.i;
+        auto [start_j, stop_j] = printing_range.j;
+        std::vector<float> pos_x(range_length.j), pos_y(range_length.i);
         while (!WindowShouldClose() && !next_step) {
             BeginDrawing();
             ClearBackground(RAYWHITE);
-            int n = dp.size(), m = dp.at(0).size();
-            for (size_t true_i = start_i; true_i < stop_i; true_i++) {
-                for (size_t true_j = start_j; true_j < stop_j; true_j++) {
-                    int i = true_i - start_i;
-                    int j = true_j - start_j;
+            //Calculate Position
+            for (int j = 0; j < range_length.j; j++) {
+                pos_x[j] = 65.0f + ((box_size + box_dist) * j);
+            }
+            for (int i = 0; i < range_length.i; i++) {
+                pos_y[i] = 50.0f + ((box_size + box_dist) * i);
+            }
+            //Draw Blue Box (Default)
+            for (int i = 0; i < range_length.i; i++) {
+                for (int j = 0; j < range_length.j; j++) {
+                    int true_i = i + start_i;
+                    int true_j = j + start_j;
                     Color color = { 170, 220, 255, 255 }; 
-                    float pos_x = 75.0f + ((box_size + box_dist) * i);
-                    float pos_y = 30.0f + ((box_size + box_dist) * j);
-                    DrawRectangleRounded({pos_x, pos_y, 100, 100}, 0.2, 6, color);
+                    DrawRectangleRounded({pos_x[j], pos_y[i], box_size, box_size}, 0.2, 6, color);
+                    DrawRectangleRoundedLinesEx({pos_x[j], pos_y[i], box_size, box_size}, 0.2, 10, 3, DARKBLUE);
                 }
             }
-            DrawTextEx(Arial, "Press SPACE for next step", {20, (float)screen_height - 30}, 20, 5, LIGHTGRAY);
+            for (auto [true_i, true_j] : cur){
+                int i = true_i - start_i;
+                int j = true_j - start_j;
+                if (i >= range_length.i || i < 0 || j >= range_length.j || j < 0) continue; //Out of bound
+                Color color = { 170, 255, 220, 255 }; //Light green
+                DrawRectangleRounded({pos_x[j], pos_y[i], box_size, box_size}, 0.2, 6, color);
+            }
+            for (auto [true_i, true_j] : prev){
+                int i = true_i - start_i;
+                int j = true_j - start_j;
+                if (i >= range_length.i || i < 0 || j >= range_length.j || j < 0) continue; //Out of bound
+                Color color = { 207, 159, 255, 255 }; //Light Violet
+                DrawRectangleRounded({pos_x[j], pos_y[i], box_size, box_size}, 0.2, 6, color);
+            }
+            for (int j = 0; j < range_length.j; j++) {
+                int true_j = j + start_j;
+                DrawTextEx(Arial, TextFormat("%d", true_j), {pos_x[j] + box_size/2 - 10,  100}, 10, 3, DARKGRAY);
+            }
+            for (int i = 0; i < range_length.i; i++) {
+                for (int j = 0; j < range_length.j; j++) {
+                    int true_i = i + start_i;
+                    int true_j = j + start_j;
+                    std::string val = std::to_string(dp[true_i][true_j]);
+                    int new_font_size = font_size - (3 * val.length());
+                    int textWidth = MeasureText(val.c_str(), new_font_size);
+                    DrawTextEx(Arial, val.c_str(), {pos_x[j] + (box_size/2 - textWidth/2), pos_y[i] + (box_size/3)}, new_font_size, 5, BLACK);
+                }
+            }
+            DrawTextEx(Arial, "Press SPACE for next step", {20, (float)screen_height - 30}, 20, 5, GRAY);
             EndDrawing();
             if (IsKeyPressed(KEY_SPACE)) next_step = true;
         }
